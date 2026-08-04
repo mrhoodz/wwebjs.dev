8f4e396bc7a12a366f1c951a9b9ecb862e6af311
realtenda <realwebsdigital@gmail.com>
2026-08-05 01:41:18 +0200

    refactor(favicon): update favicons to use SVG and ICO files

--- src/.vuepress/config/head.js
+++ src/.vuepress/config/head.js
@@ -1,7 +1,6 @@
 export const head = [
-    ['link', { rel: 'icon', href: '/images/favicon/logo_32x32.png' }],
-    ['link', { rel: 'icon', href: '/images/favicon/logo_16x16.png' }],
-    ['link', { rel: 'shortcut icon', href: '/images/favicon/logo_shortcut.ico' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/images/favicon/favicon.svg' }],
+    ['link', { rel: 'shortcut icon', href: '/images/favicon/favicon.ico' }],
     ['meta', { name: 'theme-color', content: '#25D366' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
